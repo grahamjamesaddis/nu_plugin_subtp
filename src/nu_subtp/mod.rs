ab52965081f781d7d6c60d2b384633cd7332e9ab
Graham Addis <graham@addis.org.uk>
2025-03-23 00:20:37 +0000

    More refactoring

--- src/nu_subtp/mod.rs
+++ src/nu_subtp/mod.rs
@@ -1,11 +1,11 @@
 use duplicate::duplicate_item;
-use nu_plugin::{EngineInterface, EvaluatedCall, Plugin, PluginCommand, SimplePluginCommand};
-use nu_protocol::{Category, Example, LabeledError, Record, Signature, Span, Type, Value, record};
+use nu_plugin::Plugin;
+use nu_protocol::{Record, Span, Value};
 
 mod srt;
 mod vtt;
-use srt::run_srt;
-use vtt::run_vtt;
+use crate::nu_subtp::srt::FromSrt;
+use crate::nu_subtp::vtt::FromVtt;
 pub struct SubtpPlugin;
 
 impl Plugin for SubtpPlugin {
@@ -21,129 +21,6 @@ pub trait ToValue {
     fn to_value(&self, span: Span) -> Value;
 }
 
-struct FromVtt;
-
-impl SimplePluginCommand for FromVtt {
-    type Plugin = SubtpPlugin;
-
-    fn name(&self) -> &str {
-        "from vtt"
-    }
-
-    fn signature(&self) -> nu_protocol::Signature {
-        signature(PluginCommand::name(self))
-    }
-
-    fn description(&self) -> &str {
-        "Parse text as .vtt and create records"
-    }
-
-    fn examples(&self) -> Vec<Example> {
-        examples("Convert .vtt data into records")
-    }
-
-    fn run(
-        &self,
-        _plugin: &Self::Plugin,
-        _engine: &EngineInterface,
-        call: &EvaluatedCall,
-        input: &Value,
-    ) -> Result<Value, LabeledError> {
-        run_vtt(call, input)
-    }
-}
-
-pub struct FromSrt;
-
-impl SimplePluginCommand for FromSrt {
-    type Plugin = SubtpPlugin;
-
-    fn name(&self) -> &str {
-        "from srt"
-    }
-
-    fn signature(&self) -> nu_protocol::Signature {
-        signature(PluginCommand::name(self))
-    }
-
-    fn description(&self) -> &str {
-        "Parse text as .srt and create records"
-    }
-
-    fn examples(&self) -> Vec<Example> {
-        examples("Convert .srt data into records")
-    }
-
-    fn run(
-        &self,
-        _plugin: &Self::Plugin,
-        _engine: &EngineInterface,
-        call: &EvaluatedCall,
-        input: &Value,
-    ) -> Result<Value, LabeledError> {
-        run_srt(call, input)
-    }
-}
-
-fn signature(name: &str) -> nu_protocol::Signature {
-    Signature::build(name)
-        .input_output_type(Type::String, Type::Record(Box::new([])))
-        .category(Category::Formats)
-}
-
-fn examples(description: &str) -> Vec<Example> {
-    let span = Span::test_data();
-    let vec = vec![Example {
-        description,
-        example: "WEBVTT
-
-00:01.000 --> 00:04.000
-Never drink liquid nitrogen.
-
-00:05.000 --> 00:09.000
-— It will perforate your stomach.
-— You could die.
-
-00:10.000 --> 00:14.000
-The Organisation for Sample Public Service Announcements accepts no liability for the content of this advertisement, or for the consequences of any actions taken on the basis of the information provided.
-",
-        result: Some(Value::record(
-            record! {
-                    "provider".to_string()=>Value::record(
-                        record!{
-                            "aws".to_string() => Value::record
-                            (    record!{
-                                "region".to_string()=>Value::test_string("us-east-1")
-                            },span
-                            )
-                        }
-                    ,span
-                    ),
-
-                    "resource".to_string()=> Value::record(
-                        record!{"aws_instance".to_string()=>
-                        Value::record (
-                            record!{
-                                "web".to_string()=>
-                                Value::record(
-                                    record! {
-                                        "ami".to_string()=>Value::test_string("ami-a1b2c3d4"),
-                                        "instance_type".to_string()=>Value::test_string("t2.micro"),
-                                    },
-                                    span,
-                                )
-                            },
-                            span
-                        )}
-                        ,
-                         span,
-            )
-                },
-            span,
-        )),
-    }];
-    vec
-}
 impl ToValue for Record {
     fn to_value(&self, span: Span) -> Value {
         Value::record(self.clone(), span)
--- src/nu_subtp/vtt.rs
+++ src/nu_subtp/vtt.rs
@@ -1,5 +1,9 @@
-use nu_plugin::EvaluatedCall;
-use nu_protocol::{ErrorLabel, LabeledError, Span, Value, record};
+use nu_plugin::{EngineInterface, EvaluatedCall, PluginCommand, SimplePluginCommand};
+use nu_protocol::{
+    Category, ErrorLabel, Example, LabeledError, Signature, Span, Type, Value, record,
+};
+
+use crate::nu_subtp::SubtpPlugin;
 use std::{i64, time::Duration};
 use subtp::vtt::{
     Alignment, Anchor, CueSettings, Line, LineAlignment, Percentage, Position, PositionAlignment,
@@ -8,6 +12,97 @@ use subtp::vtt::{
 };
 
 use crate::nu_subtp::ToValue;
+
+pub struct FromVtt;
+
+impl SimplePluginCommand for FromVtt {
+    type Plugin = SubtpPlugin;
+
+    fn name(&self) -> &str {
+        "from vtt"
+    }
+
+    fn signature(&self) -> nu_protocol::Signature {
+        signature(PluginCommand::name(self))
+    }
+
+    fn description(&self) -> &str {
+        "Parse text as .vtt and create records"
+    }
+
+    fn examples(&self) -> Vec<Example> {
+        examples("Convert .vtt data into records")
+    }
+
+    fn run(
+        &self,
+        _plugin: &Self::Plugin,
+        _engine: &EngineInterface,
+        call: &EvaluatedCall,
+        input: &Value,
+    ) -> Result<Value, LabeledError> {
+        run_vtt(call, input)
+    }
+}
+fn signature(name: &str) -> nu_protocol::Signature {
+    Signature::build(name)
+        .input_output_type(Type::String, Type::Record(Box::new([])))
+        .category(Category::Formats)
+}
+fn examples(description: &str) -> Vec<Example> {
+    let span = Span::test_data();
+    let vec = vec![Example {
+        description,
+        example: "WEBVTT
+
+00:01.000 --> 00:04.000
+Never drink liquid nitrogen.
+
+00:05.000 --> 00:09.000
+— It will perforate your stomach.
+— You could die.
+
+00:10.000 --> 00:14.000
+The Organisation for Sample Public Service Announcements accepts no liability for the content of this advertisement, or for the consequences of any actions taken on the basis of the information provided.
+",
+        result: Some(Value::record(
+            record! {
+                    "provider".to_string()=>Value::record(
+                        record!{
+                            "aws".to_string() => Value::record
+                            (    record!{
+                                "region".to_string()=>Value::test_string("us-east-1")
+                            },span
+                            )
+                        }
+                    ,span
+                    ),
+
+                    "resource".to_string()=> Value::record(
+                        record!{"aws_instance".to_string()=>
+                        Value::record (
+                            record!{
+                                "web".to_string()=>
+                                Value::record(
+                                    record! {
+                                        "ami".to_string()=>Value::test_string("ami-a1b2c3d4"),
+                                        "instance_type".to_string()=>Value::test_string("t2.micro"),
+                                    },
+                                    span,
+                                )
+                            },
+                            span
+                        )}
+                        ,
+                         span,
+            )
+                },
+            span,
+        )),
+    }];
+    vec
+}
+
 pub fn run_vtt(call: &EvaluatedCall, input: &Value) -> Result<Value, LabeledError> {
     let span = call.head;
     let input_string = input.as_str()?;
