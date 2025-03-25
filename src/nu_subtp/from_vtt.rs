a726a7ea87e99453d39e2d288fecb4f8b0eb7759
Graham James Addis <graham@addis.org.uk>
2025-03-25 17:09:19 +0000

    Extend help

--- src/nu_subtp/from_vtt.rs
+++ src/nu_subtp/from_vtt.rs
@@ -77,7 +77,29 @@ The Organisation for Sample Public Service Announcements accepts no liability fo
                         ], span),
                 }, span),
             }, span),
-            ],span)
+            Value::record(record!{
+                "Cue".to_string() => Value::record(record!{
+                    "Timings".to_string()=>Value::record(record!{
+                        "Start".to_string()=>Value::duration(((0*60+5)*1000+000)*1_000_000, span),
+                        "End".to_string()=>Value::duration(((0*60+9)*1000+000)*1_000_000, span),
+                    }, span),
+                "Payload".to_string()=>Value::list(vec![
+                    Value::string("— It will perforate your stomach.", span),
+                    Value::string("— You could die.", span),
+                    ], span),
+            }, span),
+        }, span),
+        Value::record(record!{
+            "Cue".to_string() => Value::record(record!{
+                "Timings".to_string()=>Value::record(record!{
+                    "Start".to_string()=>Value::duration(((0*60+10)*1000+000)*1_000_000, span),
+                    "End".to_string()=>Value::duration(((0*6014)*1000+000)*1_000_000, span),
+                }, span),
+            "Payload".to_string()=>Value::list(vec![
+                Value::string("The Organisation for Sample Public Service Announcements accepts no liability for the content of this advertisement, or for the consequences of any actions taken on the basis of the information provided.", span),
+                ], span),
+        }, span),
+    }, span),        ],span)
             }, span)
         ),
     }
