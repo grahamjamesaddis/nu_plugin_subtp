38144beb455c8e3f7da5c94ea1717ac2f5f04fab
Graham James Addis <graham@addis.org.uk>
2025-04-09 17:30:34 +0100

    Whitespace

--- src/nu_subtp/to_srt.rs
+++ src/nu_subtp/to_srt.rs
@@ -139,22 +139,6 @@ fn examples(description: &str) -> Vec<Example> {
 
 fn run(call: &EvaluatedCall, input: &Value) -> Result<Value, LabeledError> {
     let span = call.head;
-    // let input_string = input.as_str()?;
-
-    // let parse_result: SubRip =
-    //     subtp::srt::SubRip::parse(input_string).map_err(|e| LabeledError {
-    //         labels: Box::new(vec![ErrorLabel {
-    //             text: "Error parsing srt".into(),
-    //             span,
-    //         }]),
-    //         msg: e.to_string(),
-    //         code: None,
-    //         url: None,
-    //         help: None,
-    //         inner: Box::new(Vec::default()),
-    //     })?;
-
-    // Ok(NuValue::from_web_vtt(&parse_result, span))
 
     match input {
         Value::List {
