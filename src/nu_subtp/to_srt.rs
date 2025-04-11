951592ac7935bfc5657e07b8765a155c645e9820
Graham James Addis <graham@addis.org.uk>
2025-04-11 11:36:08 +0100

    Refactor to use FromValue

--- src/nu_subtp/to_srt.rs
+++ src/nu_subtp/to_srt.rs
@@ -164,10 +164,10 @@ fn timestamp_from_duration(val: i64) -> SrtTimestamp {
     let rest = rest % (1_000_000_000);
     let milliseconds = (rest / (1_000_000)) as u16;
     SrtTimestamp {
-        hours,
-        minutes,
-        seconds,
-        milliseconds,
+        hours: hours,
+        minutes: minutes,
+        seconds: seconds,
+        milliseconds: milliseconds,
     }
 }
 fn srt_construction_error(msg: &str, span: Span) -> LabeledError {
@@ -184,6 +184,61 @@ fn srt_construction_error(msg: &str, span: Span) -> LabeledError {
     }
 }
 
+trait FromValue {
+    fn get_subtitle(&self, span: Span) -> Result<SrtSubtitle, LabeledError>;
+    fn get_sequence(&self) -> Option<u32>;
+    fn get_start(&self) -> Option<SrtTimestamp>;
+    fn get_end(&self) -> Option<SrtTimestamp>;
+    fn get_text(&self) -> Option<Vec<String>>;
+}
+
+impl FromValue for Value {
+    fn get_subtitle(&self, span: Span) -> Result<SrtSubtitle, LabeledError> {
+        match self {
+            Value::Record { val, internal_span } => val.get_subtitle(*internal_span),
+            _ => Err(srt_construction_error(
+                "Input inconsistent with srt format",
+                span,
+            )),
+        }
+    }
+    fn get_sequence(&self) -> Option<u32> {
+        match self {
+            Value::Int {
+                val,
+                internal_span: _,
+            } => Some(*val as u32),
+            _ => None,
+        }
+    }
+    fn get_start(&self) -> Option<SrtTimestamp> {
+        match self {
+            Value::Duration {
+                val,
+                internal_span: _,
+            } => Some(timestamp_from_duration(*val)),
+            _ => None,
+        }
+    }
+    fn get_end(&self) -> Option<SrtTimestamp> {
+        match self {
+            Value::Duration {
+                val,
+                internal_span: _,
+            } => Some(timestamp_from_duration(*val)),
+            _ => None,
+        }
+    }
+    fn get_text(&self) -> Option<Vec<String>> {
+        match self {
+            Value::List {
+                vals,
+                internal_span: _,
+            } => text_from_list(vals),
+            _ => None,
+        }
+    }
+}
 trait FromRecord {
     fn get_subtitle(&self, span: Span) -> Result<SrtSubtitle, LabeledError>;
     fn get_sequence(&self) -> Option<u32>;
@@ -217,52 +272,28 @@ impl FromRecord for Record {
             line_position: None,
         })
     }
+    fn get_sequence(&self) -> Option<u32> {
+        self.iter()
+            .find(|(s, _)| *s == "Sequence")
+            .map(|(_, v)| v.get_sequence())
+            .unwrap_or_default()
+    }
     fn get_start(&self) -> Option<SrtTimestamp> {
         self.iter()
             .find(|(s, _)| *s == "Start")
-            .map(|(_, v)| match v {
-                Value::Duration {
-                    val,
-                    internal_span: _,
-                } => Some(timestamp_from_duration(*val)),
-                _ => None,
-            })
+            .map(|(_, v)| v.get_start())
             .unwrap_or_default()
     }
     fn get_end(&self) -> Option<SrtTimestamp> {
         self.iter()
             .find(|(s, _)| *s == "End")
-            .map(|(_, v)| match v {
-                Value::Duration {
-                    val,
-                    internal_span: _,
-                } => Some(timestamp_from_duration(*val)),
-                _ => None,
-            })
+            .map(|(_, v)| v.get_end())
             .unwrap_or_default()
     }
     fn get_text(&self) -> Option<Vec<String>> {
         self.iter()
             .find(|(s, _)| *s == "Text")
-            .map(|(_, v)| match v {
-                Value::List {
-                    vals,
-                    internal_span: _,
-                } => text_from_list(vals),
-                _ => None,
-            })
-            .unwrap_or_default()
-    }
-    fn get_sequence(&self) -> Option<u32> {
-        self.iter()
-            .find(|(s, _)| *s == "Sequence")
-            .map(|(_, v)| match v {
-                Value::Int {
-                    val,
-                    internal_span: _,
-                } => Some(*val as u32),
-                _ => None,
-            })
+            .map(|(_, v)| v.get_text())
             .unwrap_or_default()
     }
 }
@@ -281,15 +312,9 @@ fn text_from_list(list: &Vec<Value>) -> Option<Vec<String>> {
 
 impl ToSubRip for Vec<Value> {
     fn to_sub_rip(&self, span: Span) -> Result<SubRip, nu_protocol::LabeledError> {
-        let a = self.iter().to_owned().map(|val| match val {
-            Value::Record { val, internal_span } => val.get_subtitle(*internal_span),
-            _ => Err(srt_construction_error(
-                "Input inconsistent with srt format",
-                span,
-            )),
-        });
+        let subtitle_list = self.iter().to_owned().map(|val| val.get_subtitle(span));
         Ok(SubRip {
-            subtitles: a.collect::<Result<Vec<SrtSubtitle>, LabeledError>>()?,
+            subtitles: subtitle_list.collect::<Result<Vec<SrtSubtitle>, LabeledError>>()?,
         })
     }
 }
