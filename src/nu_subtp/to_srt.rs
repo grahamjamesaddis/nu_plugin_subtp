f0db21b02bfe8919f4ec644bc85b354ba785a99e
Graham James Addis <graham@addis.org.uk>
2025-04-11 11:42:50 +0100

    Clean

--- src/nu_subtp/to_srt.rs
+++ src/nu_subtp/to_srt.rs
@@ -1,7 +1,5 @@
 use nu_plugin::{EngineInterface, EvaluatedCall, PluginCommand, SimplePluginCommand};
-use nu_protocol::{
-    Category, ErrorLabel, Example, LabeledError, Record, Signature, Span, Type, Value, record,
-};
+use nu_protocol::{Category, ErrorLabel, LabeledError, Record, Signature, Span, Type, Value};
 
 use super::SubtpPlugin;
 use super::common::ToValue;
@@ -11,10 +9,6 @@ pub struct ToSrt;
 
 trait ToSubRip {
     fn to_sub_rip(&self, span: Span) -> Result<SubRip, LabeledError>;
-}
-
-trait ToSrtSubtitle {
-    fn to_sub_rip(&self, span: Span) -> Result<SrtSubtitle, LabeledError>;
 }
 
 impl SimplePluginCommand for ToSrt {
@@ -32,10 +26,6 @@ impl SimplePluginCommand for ToSrt {
         "Create .srt file from nushell records"
     }
 
-    // fn examples(&self) -> Vec<Example> {
-    //     examples("Convert .srt data into records")
-    // }
-
     fn run(
         &self,
         _plugin: &Self::Plugin,
@@ -51,90 +41,6 @@ fn signature(name: &str) -> nu_protocol::Signature {
     Signature::build(name)
         .input_output_type(Type::Table(Box::new([])), Type::String)
         .category(Category::Formats)
-}
-
-const EX_TXT: &str = "
-1 00:01:17,757 --> 00:01:18,757 Copy boy!
-
-2 00:01:20,727 --> 00:01:23,662
-Make it snappy.
-Where's the rest of this story?
-
-3 00:01:23,862 --> 00:01:25,091 Morning Post.
-
-4 00:01:25,298 --> 00:01:27,960 City desk? Just a moment and I'll connect you.
-
-5 00:01:28,268 --> 00:01:30,998 If anybody asks for me, I'm down at the courthouse.
-";
-
-fn examples(description: &str) -> Vec<Example> {
-    let span = Span::test_data();
-    let vec = vec![Example {
-        description,
-        example: EX_TXT,
-        result: Some(Value::list(
-            vec![
-                Value::record(
-                    record! {
-                        "Sequence"=>Value::int(1, span),
-                        "Start"=>Value::duration(((1*60+17)*1000+757)*1_000_000, span),
-                        "End"=>Value::duration(((1*60+18)*1000+757)*1_000_000, span),
-                        "Text"=>Value::List { vals: vec![
-                            Value::String { val:"Make it snappy.".to_string(), internal_span: span },
-                            Value::String { val:"Where's the rest of this story?".to_string(), internal_span: span },
-                            ], internal_span: span }
-                    },
-                    span,
-                ),
-                Value::record(
-                    record! {
-                        "Sequence"=>Value::int(2, span),
-                        "Start"=>Value::duration(((1*60+20)*1000+727)*1_000_000, span),
-                        "End"=>Value::duration(((1*60+23)*1000+662)*1_000_000, span),
-                        "Text"=>Value::List { vals: vec![
-                            Value::String { val:"Copy boy!".to_string(), internal_span: Span { start: 1, end: 2 } }
-                            ], internal_span: span }
-                    },
-                    span,
-                ),
-                Value::record(
-                    record! {
-                        "Sequence"=>Value::int(3, span),
-                        "Start"=>Value::duration(((1*60+23)*1000+862)*1000_000, span),
-                        "End"=>Value::duration(((1*60+25)*1000+91)*1_000_000, span),
-                        "Text"=>Value::List { vals: vec![
-                            Value::String { val:"Morning Post.".to_string(), internal_span: Span { start: 1, end: 2 } }
-                            ], internal_span: span }
-                    },
-                    span,
-                ),
-                Value::record(
-                    record! {
-                        "Sequence"=>Value::int(4, span),
-                        "Start"=>Value::duration(((1*60+25)*1000+298)*1000_000, span),
-                        "End"=>Value::duration(((1*60+27)*1000+960)*1_000_000, span),
-                        "Text"=>Value::List { vals: vec![
-                            Value::String { val:"City desk? Just a moment and I'll connect you.".to_string(), internal_span: Span { start: 1, end: 2 } }
-                            ], internal_span: span }
-                    },
-                    span,
-                ),
-                Value::record(
-                    record! {
-                        "Sequence"=>Value::int(5, span),
-                        "Start"=>Value::duration(((1*60+28)*1000+263)*1000_000, span),
-                        "End"=>Value::duration(((1*60+30)*1000+998)*1_000_000, span),
-                        "Text"=>Value::List { vals: vec![
-                            Value::String { val:"If anybody asks for me, I'm down at the courthouse.".to_string(), internal_span: Span { start: 1, end: 2 } }
-                            ], internal_span: span }
-                    },
-                    span,
-                ),
-            ],
-            span,
-        )),
-    }];
-    vec
 }
 
 fn run(call: &EvaluatedCall, input: &Value) -> Result<Value, LabeledError> {
